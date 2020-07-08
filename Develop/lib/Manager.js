964a0ca13d69201586b5038d2734072de9c9fd89
Mike Shenk <mlshenk415@gmail.com>
2020-07-07 21:11:47 -0700

    All office position tests passed

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,17 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Employee class
+const Employee = require("./Employee")
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+        this.getOfficeNumber = () => {
+            return this.officeNumber;
+        }
+        this.getRole = () => {
+            return "Manager";
+        }
+    }
+}
+
+module.exports = Manager;
